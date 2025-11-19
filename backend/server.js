a3d5b49e811581949d28558df1e33bbe781e710d
uchechukwuc <uchechukwuc@gmail.com>
2025-11-19 01:43:58 +0100

    Final Project

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://cleanguard.vercel.app" ,
   credentials: true
 }));
 
